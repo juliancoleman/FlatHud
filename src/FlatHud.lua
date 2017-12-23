d42d18e8e992fe730971fb87c9b5fd8e3f75fc3f
J. Coleman <jlcoleman3@pipeline.sbcc.edu>
2017-12-22 16:18:31 -0800

    Create FlatHud.lua

---
+++ src/FlatHud.lua
@@ -0,0 +1,20 @@
+FlatHud = {}
+
+FlatHud.name = "FlatHud"
+FlatHud.version = "0.0.1"
+
+-- Initializers --
+
+function FlatHud.OnAddOnLoaded(event, addonName)
+  if addonName ~= FlatHud.name then return end
+  
+  FlatHud:Initialize()
+end
+
+function FlatHud:Initialize()
+  EVENT_MANAGER:UnregisterForEvent(FlatHud.name, EVENT_ADD_ON_LOADED)
+end
+
+-- Register Events --
+
+EVENT_MANAGER:RegisterForEvent(FlatHud.name, EVENT_ADD_ON_LOADED, FlatHud.OnAddonLoaded)
